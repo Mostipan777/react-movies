d7d60a51bd87a136389e5b3cede8d08171ac78ea
Mostipan777 <mostipan777@gmail.com>
2021-03-23 14:12:59 +0200

    Updates

--- src/components/Preloader.js
+++
@@ -1,9 +0,0 @@
-function Preloader() {
-  return (
-    <div className="progress grey lighten-2">
-      <div className="indeterminate deep-purple darken-4"></div>
-    </div>
-  );
-}
-
-export { Preloader };
